2ad764f7816841a4a0f534a28a3f1848bf90aed4
melanie zurdo <melaniezurdo1995@gamil.com>
2022-02-23 23:09:09 -0300

    23/02

--- Main/Model_main/PacDot.py
+++
@@ -1,7 +0,0 @@
-from EatableEntity import EatableEntity
-
-class PacDot(EatableEntity):
-    def __init__(self):
-        self.shape="*"
-    def __str__(self):
-        return "["+str(self.shape)+"]"
